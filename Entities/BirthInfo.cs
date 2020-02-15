3637bdba1519b463ffb565426cff0d558e8ca328
Zhenia Borodaikevych <borodaykevich@gmail.com>
2020-02-16 00:30:56 +0200

    moved Birth Date related properties to separate class

---
+++ Entities/BirthInfo.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BorodaikevychZodiac.Entities
+{
+  public class BirthInfo
+  {
+    private DateTime? _birthDate;
+
+
+    public string BirthDateString
+    {
+      set
+      {
+        DateTime.TryParseExact(value, "dd-MM-yyyy", CultureInfo.InvariantCulture,
+          DateTimeStyles.None, out var date);
+
+        _birthDate = date;
+      }
+
+      get
+      {
+        if (_birthDate == null)
+        {
+          return null;
+        }
+
+        var date = _birthDate.Value.Date;
+
+        if (date == DateTime.MinValue)
+        {
+          return "";
+        }
+
+        return date.ToString("dd-MM-yyyy");
+      }
+    }
+
+    public int Age
+    {
+      get
+      {
+        if (string.IsNullOrEmpty(BirthDateString) || _birthDate == null) return -1;
+        var today = DateTime.Now;
+        var age = today.Year - _birthDate.Value.Year;
+        if (_birthDate.Value.Date > today.AddYears(-age)) age--;
+        return age;
+      }
+    }
+
+    public bool IsBornToday
+    {
+      get
+      {
+        if (_birthDate == null) return false;
+        var date = _birthDate.Value.Date;
+        var today = DateTime.Today;
+        return date.Day == today.Day && date.Month == today.Month;
+      }
+    }
+  }
+}
--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -5,6 +5,7 @@ using System.Globalization;
 using System.Linq;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using BorodaikevychZodiac.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.TagHelpers;
@@ -14,60 +15,15 @@ namespace BorodaikevychZodiac.Pages
 {
   public class IndexModel : PageModel
   {
-    private DateTime? _birthDate;
-
     [BindProperty]
     [Required]
     [DisplayFormat(NullDisplayText = "", DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
-    public string BirthDateString
+    public string BirthDateInput
     {
-      set
-      {
-        DateTime.TryParseExact(value, "dd-MM-yyyy", CultureInfo.InvariantCulture,
-          DateTimeStyles.None, out var date);
-
-        _birthDate = date;
-      }
-
-      get
-      {
-        if (_birthDate == null)
-        {
-          return null;
-        }
-
-        var date = _birthDate.Value.Date;
-
-        if (date == DateTime.MinValue)
-        {
-          return "";
-        }
-
-        return date.ToString("dd-MM-yyyy");
-      }
+      get => BirthInfo.BirthDateString;
+      set => BirthInfo.BirthDateString = value;
     }
 
-    public int Age
-    {
-      get
-      {
-        if (string.IsNullOrEmpty(BirthDateString)) return -1;
-        var today = DateTime.Now;
-        var age = today.Year - _birthDate.Value.Year;
-        if (_birthDate.Value.Date > today.AddYears(-age)) age--;
-        return age;
-      }
-    }
-
-    public bool IsBornToday
-    {
-      get
-      {
-        if (_birthDate == null) return false;
-        var date = _birthDate.Value.Date;
-        var today = DateTime.Today;
-        return date.Day == today.Day && date.Month == today.Month;
-      }
-    }
+    public BirthInfo BirthInfo { get; } = new BirthInfo();
   }
 }
