b9d05c40a79fd2c55b26d67e20a4ec8ea1f72037
Zhenia Borodaikevych <borodaykevich@gmail.com>
2020-02-15 20:06:14 +0200

    Add project files.

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace BorodaikevychZodiac.Pages
+{
+  public class IndexModel : PageModel
+  {
+    private readonly ILogger<IndexModel> _logger;
+
+    public IndexModel(ILogger<IndexModel> logger)
+    {
+      _logger = logger;
+    }
+
+    [DataType(DataType.Date)]
+    public DateTime? BirthDate { get; private set; }
+
+    public IActionResult OnPost(string birthDate)
+    {
+      DateTime.TryParseExact(birthDate, "dd-MM-yyyy", CultureInfo.InvariantCulture,
+        DateTimeStyles.None, out var date);
+
+      BirthDate = date;
+
+
+      return Page();
+    }
+  }
+}
---
+++ wwwroot/js/site.js
@@ -0,0 +1,17 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+
+$(() => {
+    $(".datepicker").datepicker({
+        clearBtn: true,
+        format: "dd-mm-yyyy",
+        endDate: "0d",
+        startDate: "-135y",
+        weekStart: 1,
+        onClose: true,
+        immediateUpdates: true
+        //showOnFocus: false
+    });
+})
