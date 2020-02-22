326b2da72e684df9ce92aa398bf100952d308175
Zhenia Borodaikevych <borodaykevich@gmail.com>
2020-02-22 21:26:39 +0200

    moved logic from controller

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,42 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Globalization;
-using System.Linq;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
-using BorodaikevychZodiac.Entities;
+﻿using System.ComponentModel.DataAnnotations;
+using BorodaikevychZodiac.Models.User;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.AspNetCore.Mvc.TagHelpers;
-using Microsoft.Extensions.Logging;
 
 namespace BorodaikevychZodiac.Pages
 {
   public class IndexModel : PageModel
   {
     [BindProperty]
-    public string BirthDateString
-    {
-      set
-      {
-        DateTime.TryParseExact(value, "dd-MM-yyyy", CultureInfo.InvariantCulture,
-          DateTimeStyles.None, out var date);
-
-
-        BirthInfo.BirthDate = date <= DateTime.Today ? date : default;
-
-        IsFirstAttempt = false;
-      }
-
-      get => BirthInfo.BirthDate == DateTime.MinValue ? "" : BirthInfo.BirthDate.ToString("dd-MM-yyyy");
-    }
-
-    public bool IsFirstAttempt { get; private set; } = true;
-
-    public BirthInfo BirthInfo { get; } = new BirthInfo();
-
-    public (string name, string emoji) ChineseZodiacSign => ZodiacSigns.ChineseSign(BirthInfo.BirthDate);
-    public (string name, string emoji) WesternZodiacSign => ZodiacSigns.WesternSign(BirthInfo.BirthDate);
+    [Required]
+    [ValidBirthDate]
+    public new UserModel User { get; set; }
   }
 }
