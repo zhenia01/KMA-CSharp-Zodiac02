148c5d396e80054e0e134d0dd54dd96c434a4ba0
Zhenia Borodaikevych <borodaykevich@gmail.com>
2020-02-22 21:21:11 +0200

    Added model for user

---
+++ Models/User/UserModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+using BorodaikevychZodiac.Entities;
+
+namespace BorodaikevychZodiac.Models.User
+{
+  public class UserModel
+  {
+    private readonly BirthInfo _birthInfo = new BirthInfo();
+
+    public string BirthDate
+    {
+      set
+      {
+        if (DateTime.TryParseExact(value, "dd-MM-yyyy", CultureInfo.InvariantCulture,
+          DateTimeStyles.None, out var birthDate))
+        {
+          _birthInfo.BirthDate = birthDate;
+        }
+
+        ChineseZodiacSign = ZodiacSigns.ChineseSign(_birthInfo.BirthDate);
+        WesternZodiacSign = ZodiacSigns.WesternSign(_birthInfo.BirthDate);
+      }
+
+      get => _birthInfo != default ? _birthInfo.BirthDate.ToString("dd-MM-yyyy") : "";
+    }
+
+    public int Age => _birthInfo.Age;
+
+    public bool IsBornToday => _birthInfo.IsBornToday;
+
+    public (string name, string emoji) ChineseZodiacSign { get; private set; }
+    public (string name, string emoji) WesternZodiacSign { get; private set; }
+  }
+}
---
+++ Models/User/ValidBirthDateAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BorodaikevychZodiac.Models.User
+{
+  public class ValidBirthDateAttribute : ValidationAttribute
+  {
+    public string GetErrorMessage() => "Birth Date is not valid";
+
+    public override bool IsValid(object value)
+    {
+      if (value is UserModel user)
+      {
+        return user.BirthDate != "";
+      }
+      return false;
+    }
+  }
+}
